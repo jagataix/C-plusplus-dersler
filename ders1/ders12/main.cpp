36c60af03adfd80dca223feb2e37d596c17d6c8e
jagataix <149905544+jagataix@users.noreply.github.com>
2024-07-09 22:28:28 +0300

    Add files via upload

---
+++ ders1/ders12/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+int main()
+{
+    //kosul ifadeleri 'C' ile ayni
+
+    if(19>7){
+        cout<<"19, 7'den buyuktur.\n";
+    }
+
+    if(19<7){
+        cout<<"19, 7'den kucuk.";
+    }
+    else{
+        cout<<"19, 7'den kucuk degildir.";
+    }
+
+
+
+
+
+
+
+    return 0;
+}
